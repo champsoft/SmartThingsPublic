ea812773f33a026063e1c645c9acac9663c469f4
pchampsoft <pascal.longchamp@verizon.net>
2017-11-03 07:36:19 -0400

    Added flexibility to allow a timer to start based on a switch's ON of OFF status.

--- smartapps/champsoft/off-switching-delay.src/off-switching-delay.groovy
+++ smartapps/champsoft/off-switching-delay.src/off-switching-delay.groovy
@@ -17,7 +17,7 @@ definition(
     name: "Off Switching Delay",
     namespace: "champsoft",
     author: "pchampsoft",
-    description: "Turn on/off target switch(es) when a trigger switch is powered on/off. The target switches are delayed when the trigger switch is turning off.",
+    description: "Turn on/off target switch(es) when a trigger switch is powered on/off. The target switches are delayed when the trigger switch is turning on/off.",
     category: "Convenience",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
@@ -25,19 +25,20 @@ definition(
 
 
 preferences {
-	section("Turn switch ON also") {
-    	input "onAlso", "bool", title: "Switch devices on as well?", required: true, defaultValue: true
+	section("Switch target device ON also") {
+    	input "targetsOnAlso", "bool", title: "Switch devices on as well?", required: true, defaultValue: true
     }
-    
     section("Select the target switches that you want to delay on power off") {
         input "targetSwitches", "capability.switch", title: "Target Switches", required: true, multiple: true
         input "delay", "number", title: "Switched Off Delay After Trigger is Switched Off (seconds)", required: true, 
         	defaultValue: "30"
 	}
-    
     section("Select the the trigger switch") {
         input "triggerSwitch", "capability.switch", title: "Trigger Switch", required: true
 	}
+    section("Trigger State that triggers the delay") {
+    		input "triggerState", "bool", title: "OFF or ON?", required: true, defaultValue: false
+    }
 }
 
 def installed() {
@@ -56,29 +57,47 @@ def updated() {
 def initialize() {
 	subscribe(triggerSwitch, "switch.on", switchOnHandler)
     subscribe(triggerSwitch, "switch.off", switchOffHandler)
+    //state to indicate when the timer was initiated when the trigger device was switched on
+    if(atomicState.isProcessed == null)
+    	atomicState.isProcessed = false
 }
 
 def switchOnHandler(evt) {
-	//If there is an active off switch timer, cancel it
-    unschedule(turnSwitchesOff)
-	turnSwitchesOn()
+	//If the user selects that the target should also turn on when the trigger device is switched on
+    if(targetsOnAlso) {
+       	turnSwitchesOn()
+        log.trace "Turning switches ON."
+    }
+    //If user selects the trigger device's ON state to start the target's OFF timer
+    if(triggerState) {
+    	atomicState.isProcessed = true
+    	switchOffHandler(evt)
+    }
 }
 
 def switchOffHandler(evt) {
-  	runIn(delay, turnSwitchesOff)
-   	log.trace "Starting OFF timer."
+	if(evt.value == "off" && atomicState.isProcessed == true) {
+    	log.trace "Attempt made to turn off switch while it may already be on a schedule. Cancelling any schedules... "
+    	unschedule(turnSwitchesOff)
+        atomicState.isProcessed = false
+        return
+    }
+    
+    runIn(delay, turnSwitchesOff)
+   	log.trace "Starting OFF timer from event ${evt.value}"
 }
 
 def turnSwitchesOn() {
-    if(onAlso) {
-    	targetSwitches.findAll {
-    		if (it.currentSwitch == "off") {
-        		log.debug "Switching ON: ${it.label}"
-  	       		it.on()
-       	 	}
-        	else
-        		log.debug "Switch $it.label is already on, OFF timer canceled"
-    	}
+	//If there is an active off switch timer, cancel it
+    unschedule(turnSwitchesOff)
+    
+    targetSwitches.findAll {
+    	if (it.currentSwitch == "off") {
+        	log.debug "Switching ON: ${it.label}"
+  	    	it.on()
+       	}
+        else
+        	log.debug "Switch $it.label is already on, Do nothing."
     }
 }
 
